5a7973ad9542168cbfa850fac8c7ef472fd9f3db
chujonghyun <chuwhdgus@gmail.com>
2022-12-06 13:18:26 +0900

    Top competitors 블로그 주석 추가

--- SQL/HackerRank/Top Competitors.sql
+++ SQL/HackerRank/Top Competitors.sql
@@ -1,4 +1,5 @@
 -- problem url = "https://www.hackerrank.com/challenges/full-score/problem?h_r=profile"
+-- 해결 과정 = "https://velog.io/@jonghyun3668/SQL-%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4-HackerRank-Top-Competitors"
 
 SELECT Ha.hacker_id, Ha.name
 FROM Submissions Sub
