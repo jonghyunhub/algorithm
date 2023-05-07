4bc1a1d2598f53a4ec688467a70d117a31fd8f0e
jonghyunhub <chuwhdgus@gmail.com>
2023-05-07 16:28:12 +0900

    feat : 2743 단어 길이 재기 풀이 추가

---
+++ src/boj/BOJ2743.java
@@ -0,0 +1,15 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2743 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(s.length());
+        br.close();
+    }
+}
