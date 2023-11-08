e62a2b6aae1e69e3cc8de85a7f7496f55fd3b1af
jonghyunhub <chuwhdgus@gmail.com>
2023-11-08 22:44:42 +0900

    feat : 코딩 테스트 준비 - 그래프 알고리즘 문제 풀이 추가

---
+++ src/prepare_coding_test_problem/graph/BOJ2252/BOJ2252DFS.java
@@ -0,0 +1,41 @@
+package prepare_coding_test_problem.graph.BOJ2252;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BOJ2252DFS {
+    static ArrayList<Integer>[] a;
+    static boolean[] check;
+
+    static void go(int x) {
+        check[x] = true;
+        for (int y : a[x]) {
+            if (check[y] == false) {
+                go(y);
+            }
+        }
+        System.out.print(x + " ");
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        a = new ArrayList[n + 1];
+        check = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            a[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            a[y].add(x);
+        }
+        for (int i = 1; i <= n; i++) {
+            if (check[i] == false) {
+                go(i);
+            }
+        }
+        System.out.println();
+    }
+}
