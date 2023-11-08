0a9bff0fa6d30c48ab65e054e488a494c4094990
jonghyunhub <chuwhdgus@gmail.com>
2023-11-08 22:54:42 +0900

    feat : 코딩 테스트 준비 - BFS 문제 풀이 추가

---
+++ src/prepare_coding_test_problem/BFS/BOJ12851.java
@@ -0,0 +1,40 @@
+package prepare_coding_test_problem.BFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BOJ12851 {
+    public static final int MAX = 1000000;
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        boolean[] c = new boolean[MAX];
+        int[] d = new int[MAX];
+        int[] cnt = new int[MAX];
+        c[n] = true;
+        d[n] = 0;
+        cnt[n] = 1;
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.add(n);
+        while (!q.isEmpty()) {
+            int now = q.remove();
+            for (int next : new int[]{now - 1, now + 1, now * 2}) {
+                if (next >= 0 && next < MAX) {
+                    if (c[next] == false) {
+                        q.add(next);
+                        c[next] = true;
+                        d[next] = d[now] + 1;
+                        cnt[next] = cnt[now];
+                    } else if (d[next] == d[now] + 1) {
+                        cnt[next] += cnt[now];
+                    }
+                }
+            }
+        }
+        System.out.println(d[m]);
+        System.out.println(cnt[m]);
+    }
+}
