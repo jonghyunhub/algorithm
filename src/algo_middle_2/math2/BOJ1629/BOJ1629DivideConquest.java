a49a308336782ab4d929daa6edf15afcfc0d812e
jonghyunhub <chuwhdgus@gmail.com>
2023-11-10 15:43:23 +0900

    feat : 코딩테스트 준비 기초 - 시뮬레이션 문제 풀이 추가

--- src/algo_middle_2/math2/BOJ1629/BOJ1629DivideConquest.java
+++ src/algo_middle_2/math2/BOJ1629/BOJ1629DivideConquest.java
@@ -2,26 +2,26 @@ package algo_middle_2.math2.BOJ1629;
 
 import java.util.Scanner;
 
-public class BOJ1629DivideConquest
+public class BOJ1629DivideConquest {
 
-        static long go(long a, long b, long c) {
-            if (b == 0) {
-                return 1;
-            } else if (b == 1) {
-                return a % c;
-            } else if (b % 2 == 0) {
-                long temp = go(a, b / 2, c);
-                return (temp * temp) % c;
-            } else {
-                return (a * go(a, b - 1, c)) % c;
-            }
+    static long go(long a, long b, long c) {
+        if (b == 0) {
+            return 1;
+        } else if (b == 1) {
+            return a % c;
+        } else if (b % 2 == 0) {
+            long temp = go(a, b / 2, c);
+            return (temp * temp) % c;
+        } else {
+            return (a * go(a, b - 1, c)) % c;
         }
+    }
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            long a = sc.nextLong();
-            long b = sc.nextLong();
-            long c = sc.nextLong();
-            System.out.println(go(a, b, c));
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+        System.out.println(go(a, b, c));
+    }
 }
