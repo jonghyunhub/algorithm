b64c03db75224e6aa24229c40c1540a011d8af70
jonghyunhub <chuwhdgus@gmail.com>
2024-05-08 19:18:14 +0900

    feat : 배열 뒤집기 풀이

---
+++ src/programmers/array/ReverseArray.java
@@ -0,0 +1,17 @@
+package programmers.array;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        
+    }
+    public int[] solution(int[] numList) {
+        List<Integer> list = Arrays.stream(numList).boxed().collect(Collectors.toList());
+        Collections.reverse(list);
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
