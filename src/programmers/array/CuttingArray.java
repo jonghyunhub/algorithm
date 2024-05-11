4a946943504813adb3a2bd967b3dea768a38c3c2
jonghyunhub <chuwhdgus@gmail.com>
2024-05-11 19:12:12 +0900

    feat : n^2 배열 나누기 수식 풀이 추가

--- src/programmers/array/CuttingArray.java
+++ src/programmers/array/CuttingArray.java
@@ -1,5 +1,8 @@
 package programmers.array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // https://school.programmers.co.kr/learn/courses/30/lessons/87390
 public class CuttingArray {
 
@@ -7,13 +10,36 @@ public class CuttingArray {
         CuttingArray cuttingArray = new CuttingArray();
         int n = 3;
         long left = 2, right = 5;
-        int[] result = cuttingArray.solution(n, left, right);
+        int[] result = cuttingArray.solution2(n, left, right);
     }
 
-    public int[] solution(int n, long left, long right) {
+
+    // 실제 배열 구현 풀이 -> 문제 인자가 너무 많아서 메모리 초과 일어남
+    // 1 ≤ n ≤ 10^7
+    // 0 ≤ left ≤ right < n^2
+    // right - left < 105
+    public int[] solution1(int n, long left, long right) {
         int[][] init = initInput(n);
         int[] arr = makeSingleDimensionList(n, init);
         return cutArr(left, right, arr);
+    }
+
+
+    /**
+     * 수식 풀이
+     * @param n
+     * @param left : left = p1 * n + q1,  arr[p1][q1]
+     * @param right : right = p2 * n + q2, arr[p2][q2]
+     * @return
+     */
+    public int[] solution2(int n, long left, long right) {
+        List<Long> answer = new ArrayList<>();
+        for(long idx = left; idx <= right; idx++) {
+            long p = idx / n + 1;
+            long q = idx % n + 1;
+            answer.add(Math.max(p, q));
+        }
+        return answer.stream().mapToInt(Long::intValue).toArray();
     }
 
 
