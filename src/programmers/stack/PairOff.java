f03554bac8e83f083e7522da3838212b7e705550
jonghyunhub <chuwhdgus@gmail.com>
2024-05-16 19:44:05 +0900

    feat : 짝지어 제거하기 문제 풀이추가

---
+++ src/programmers/stack/PairOff.java
@@ -0,0 +1,35 @@
+package programmers.stack;
+
+import java.util.Stack;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12973
+public class PairOff {
+    // abccba -> abba -> aa
+    public int solution(String s) {
+        String input = s;
+        while (!input.isEmpty()) {
+            String nextString = proceedPairOff(s);
+            if (input.equals(nextString))
+                return 0;
+            input = nextString;
+        }
+
+        return 1;
+    }
+
+    private String proceedPairOff(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek().equals(c)) {
+                stack.pop();
+                continue;
+            }
+            stack.push(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Character c : stack) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
