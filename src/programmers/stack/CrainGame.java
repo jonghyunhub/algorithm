e9375169723eb55bb3381799a6c2763df0a487d6
jonghyunhub <chuwhdgus@gmail.com>
2024-08-09 00:03:52 +0900

    크레인 인형뽑기 게임 문제 풀이 추가

---
+++ src/programmers/stack/CrainGame.java
@@ -0,0 +1,54 @@
+package programmers.stack;
+
+import java.util.Stack;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/64061?language=java
+public class CrainGame {
+
+    public static void main(String[] args) {
+        final CrainGame crainGame = new CrainGame();
+        final int[][] board = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 3},
+                {0, 2, 5, 0, 1},
+                {4, 2, 4, 4, 2},
+                {3, 5, 1, 3, 1}
+        };
+        final int[] moves = {1, 5, 3, 5, 1, 2, 1, 4};
+        System.out.println(crainGame.solution(board, moves));
+    }
+
+    public int solution(int[][] board, int[] moves) {
+        final Stack<Integer> basket = new Stack<>();
+        final Stack<Integer>[] toyContainers = initToyContainers(board);
+        int answer = 0;
+
+        for (int move : moves) {
+            if (toyContainers[move - 1].isEmpty()) continue;
+            Integer toyNumber = toyContainers[move - 1].pop();
+            if (!basket.isEmpty() && toyNumber.equals(basket.peek())) {
+                basket.pop();
+                answer += 2;
+            } else {
+                basket.push(toyNumber);
+            }
+        }
+
+
+        return answer;
+    }
+
+    private Stack<Integer>[] initToyContainers(int[][] board) {
+        final Stack<Integer>[] toyContainers = new Stack[board.length];
+        for (int i = 0; i < board.length; i++) {
+            toyContainers[i] = new Stack<>();
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j= board.length - 1; j >0; j--) {
+                if (board[j][i] != 0) toyContainers[i].push(board[j][i]);
+            }
+        }
+        return toyContainers;
+    }
+}
