c9c253d660fcebacc99ce29aa2169d34cc59d6af
jonghyunhub <chuwhdgus@gmail.com>
2024-10-01 17:52:11 +0900

    feat : 컨트롤 제트 문제 풀이 추가

---
+++ src/programmers/stack/ControlZ.java
@@ -0,0 +1,30 @@
+package programmers.stack;
+
+import java.util.*;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120853?language=java
+public class ControlZ {
+
+    /**
+     1. 문자열을 파싱한다.
+     2. 스택에 파싱한 배열을 순회하면서 넣는다.
+     3. if(넣는 문자 == "Z") -> stack.pop();
+
+     [시간 복잡도]
+     문자열 s를 파싱한 inputs의 길이 : n 일때
+     O(n)
+     */
+    public int solution(String s) {
+        String[] inputs = s.split(" ");
+        final Stack<Integer> stack = new Stack<>();
+        for(String input : inputs){
+            if(input.equals("Z")){
+                stack.pop();
+                continue;
+            }
+            stack.push(Integer.parseInt(input));
+        }
+
+        return stack.stream().mapToInt(Integer::intValue).sum();
+    }
+}
