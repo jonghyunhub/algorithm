7ec24bf6cd8fd447ba008b5408e5850b872778f5
jonghyunhub <chuwhdgus@gmail.com>
2024-12-23 01:14:25 +0900

    docs : 문제 링크 주석 추가

--- src/programmers/queue/TruckInBridge.java
+++ src/programmers/queue/TruckInBridge.java
@@ -2,6 +2,7 @@ package programmers.queue;
 
 import java.util.*;
 
+// https://school.programmers.co.kr/learn/courses/30/lessons/42583
 public class TruckInBridge {
 
     public static void main(String[] args) {
