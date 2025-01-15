6f6a3ce4dee558cd752c0208a945a8053b61e548
jonghyunhub <chuwhdgus@gmail.com>
2025-01-16 00:40:35 +0900

    feat : 게임맵 최단거리 문제풀이 추가

--- src/programmers/graph/GameMapShortestPath.java
+++ src/programmers/graph/GameMapShortestPath.java
@@ -1,8 +1,6 @@
 package programmers.graph;
 
-import java.util.ArrayDeque;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/1844
 public class GameMapShortestPath {
@@ -24,7 +22,7 @@ public class GameMapShortestPath {
         };
 
         GameMapShortestPath gameMapShortestPath = new GameMapShortestPath();
-        int solution = gameMapShortestPath.solution(map2);
+        int solution = gameMapShortestPath.solution(map1);
         System.out.println("solution = " + solution);
     }
 
@@ -44,29 +42,29 @@ public class GameMapShortestPath {
             final Node poll = queue.poll();
             for (char moveKey : move.keySet()) {
                 Node moveValue = move.get(moveKey);
-                int newCol = poll.col + moveValue.col;
                 int newRow = poll.row + moveValue.row;
+                int newCol = poll.col + moveValue.col;
 
                 // 맵 밖으로 벗어남 처리
-                if (newCol < 0 || newCol >= maps.length || newRow < 0 || newRow >= maps[0].length) {
+                if (newRow < 0 || newRow >= maps.length || newCol < 0 || newCol >= maps[0].length) {
                     continue;
                 }
 
                 // 벽처리
-                if (maps[newCol][newRow] == 0) {
+                if (maps[newRow][newCol] == 0) {
                     continue;
                 }
 
                 // 현재 위치가 처음 방문한 경우에는 큐에 추가
-                if (dists[newCol][newRow] == 0) {
-                    queue.add(new Node(newCol, newRow));
-                    dists[newCol][newRow] = dists[poll.col][poll.row] + 1;
+                if (dists[newRow][newCol] == 0) {
+                    queue.add(new Node(newRow, newCol));
+                    dists[newRow][newCol] = dists[poll.row][poll.col] + 1;
                     continue;
                 }
 
                 // 현재 위치의 거리가 기존보다 짧게 올 수 있으면 값을 갱신
-                if (dists[newCol][newRow] > dists[poll.col][poll.row] + 1) {
-                    dists[newCol][newRow] = dists[poll.col][poll.row] + 1;
+                if (dists[newRow][newCol] > dists[poll.row][poll.col] + 1) {
+                    dists[newRow][newCol] = dists[poll.row][poll.col] + 1;
                 }
             }
         }
@@ -76,13 +74,53 @@ public class GameMapShortestPath {
     }
 
     public static class Node {
-        int col;
         int row;
+        int col;
 
-        Node(int col, int row) {
-            this.col = col;
+        Node(int row, int col) {
             this.row = row;
+            this.col = col;
         }
+    }
+
+    public int solution2(int[][] maps) {
+        final Map<String, List<Integer>> move = Map.of(
+                "UP", List.of(-1, 0),
+                "DOWN", List.of(1, 0),
+                "RIGHT", List.of(0, 1),
+                "LEFT", List.of(0, -1)
+        );
+        // 방문 처리 및 거리 저장할 2차원 배열 추가 및 초기화
+        int[][] visited = new int[maps.length][maps[0].length];
+        for (int[] row : visited)
+            Arrays.fill(row, -1);
+
+        final Queue<Node> queue = new ArrayDeque<>();
+        // 시작점 넣고 방문처리
+        queue.add(new Node(0, 0));
+        visited[0][0] = 1;
+
+        // BFS 탐색
+        while (!queue.isEmpty()) {
+            final Node now = queue.poll();
+            for (List<Integer> nextMove : move.values()) {
+                final int nextRow = now.row + nextMove.get(0);
+                final int nextCol = now.col + nextMove.get(1);
+                // 맵에서 벗어난 경우 스킵
+                if (nextRow < 0 || nextRow >= maps.length || nextCol < 0 || nextCol >= maps[0].length)
+                    continue;
+                // 벽인 경우 스킵
+                if (maps[nextRow][nextCol] == 0) continue;
+                // 이미 방문한 노드인 경우 스킵
+                if (visited[nextRow][nextCol] != -1) continue;
+                // 방문처리
+                visited[nextRow][nextCol] = visited[now.row][now.col] + 1;
+                // 큐에 삽입
+                queue.add(new Node(nextRow, nextCol));
+            }
+        }
+
+        return visited[visited.length - 1][visited[0].length - 1];
     }
 
 
