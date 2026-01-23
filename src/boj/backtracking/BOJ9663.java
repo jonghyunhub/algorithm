6fa0c287a62e14513e6ef963a2da04e0c11090a4
jonghyunhub <chuwhdgus@gmail.com>
2026-01-23 11:21:02 +0900

    백준 n queen 문제 다시 풀이

--- src/boj/backtracking/BOJ9663.java
+++ src/boj/backtracking/BOJ9663.java
@@ -3,8 +3,7 @@ package boj.backtracking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 
 // https://www.acmicpc.net/problem/9663
@@ -16,13 +15,9 @@ public class BOJ9663 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
-        List<Node> tmp = new ArrayList<>();
-        for (int col = 0; col < n; col++) {
-            Node start = new Node(0, col);
-            tmp.add(start);
-            backTracking(n, tmp, start);
-            tmp.remove(tmp.size() - 1);
-        }
+        int[] columns = new int[n];
+        Arrays.fill(columns, -1);
+        backTracking(0, n, columns);
         System.out.println(answer);
     }
 
@@ -32,50 +27,34 @@ public class BOJ9663 {
      * 1. 같은 col인 Node가 있으면 X
      * 2. 대각선 왼쪽 위 Node 가 있으면 X -> (row -k , col - k) = (row,col)
      * 3. 대각선 오른쪽 위 Node 가 있으면 X -> (row -k , col + k) = (row,col)
+     * columns[0] = 1 => row = 0 , col = 1 에 퀸 위치
      */
-    static public void backTracking(int n,
-                                    List<Node> tmp,
-                                    Node now) {
-        if (now.row == n - 1) {
+    static public void backTracking(int row ,int n, int[] columns) {
+        if(row >= n) {
             answer++;
             return;
         }
 
-        for (int col = 0; col < n; col++) {
-            Node next = new Node(now.row + 1, col);
-            if (!canAddQueen(tmp, next)) continue;
-            tmp.add(next);
-            backTracking(n, tmp, next);
-            tmp.remove(tmp.size() - 1);
+        for(int col = 0; col < n; col++) {
+            if(!canMakeQueen(row, col, columns)) continue;
+            columns[row] = col;
+            backTracking(row + 1, n, columns);
+            columns[row] = -1;
         }
-
     }
 
-
-    static public boolean canAddQueen(List<Node> tmp, Node now) {
-        for (Node node : tmp) {
-            if (node.row == now.row) {
+    static public boolean canMakeQueen(int nowRow, int nowCol, int[] columns){
+        for(int row=0; row<columns.length; row++) {
+            if(columns[row] == -1) continue;
+            if(row == nowRow || columns[row] == nowCol) {
                 return false;
             }
-
-            if (node.col == now.col) {
-                return false;
-            }
-
-            if (Math.abs(node.col - now.col) == Math.abs(node.row - now.row)) {
+            if(Math.abs(nowRow - row) == Math.abs(nowCol - columns[row])) {
                 return false;
             }
         }
+
         return true;
     }
 
-    static class Node {
-        int row;
-        int col;
-
-        public Node(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-    }
 }
